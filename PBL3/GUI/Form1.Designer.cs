7450fffe10ae5872449b8cbeab3b57433a6529ed
imlawcs <daolehanhnguyen@gmail.com>
2024-04-20 14:59:49 +0700

    hi

--- PBL3/GUI/Form1.Designer.cs
+++ PBL3/GUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PBL3.GUI
 {
-    partial class DangNhap
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // DangNhap
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "DangNhap";
-            this.Text = "DangNhap";
-            this.ResumeLayout(false);
-
+            this.Text = "Form1";
         }
 
         #endregion
