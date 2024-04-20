34e178f39aa514b1bf0bc892a6113bcdae7d9753
Administrator <Administrator@DESKTOP-BI97NJV>
2024-04-20 16:58:34 +0700

    hoa1

--- PBL3/DTO/CaTruc.cs
+++ PBL3/DTO/CaTruc.cs
@@ -9,15 +9,11 @@ namespace PBL3.DTO
     internal class CaTruc
     {
         public int MaCT {  get; set; }
-<<<<<<< HEAD
+
         public DateTime NgatTruc { get; set; }
         public string ThoiGianBD { get; set; }
         public string ThoiGianKT { get; set; }
-=======
-        public DateTime NgayTruc { get; set; }
-        public TimeSpan ThoiGianBD { get; set; }
-        public TimeSpan ThoiGianKT { get; set; }
->>>>>>> d616ce3b47d6677877e9380d77845868a58e7a59
+
         public double DoanhThu {  get; set; }
 
         public ICollection<NhanVien> NhanViens { get; set; }
