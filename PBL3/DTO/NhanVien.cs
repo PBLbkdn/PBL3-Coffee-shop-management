80a26b76b4350873853224d83acf2c9525888d10
Monmoonluna <minhnguyetdn2004@gmail.com>
2024-04-20 15:18:32 +0700

    đã kết nối được csdl

--- PBL3/DTO/NhanVien.cs
+++ PBL3/DTO/NhanVien.cs
@@ -9,11 +9,11 @@ namespace PBL3.DTO
     internal class NhanVien
     {
         public int MaNV {  get; set; }
-        public int MaVC { get; set; }
+        public int MaCV { get; set; }
         public string HoTenNV { get; set; }
         public DateTime NgaySinh { get; set; }
-        public string Sdt {  get; set; }
+        public string SDT {  get; set; }
         public double Luong {  get; set; }
-
+        public string GioiTinh { get; set; }
     }
 }
--- PBL3/GUI/Form1.cs
+++ PBL3/GUI/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using PBL3.DAO;
+using PBL3.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +19,24 @@ namespace PBL3.GUI
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<NhanVien> list = new List<NhanVien>();
+            string s = @"Data Source=DESKTOP-9SBKOG0\NGUYET;Initial Catalog=QuanCaPhe;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(s);
+            string query = "select * from NhanVien";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cnn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dr);
+            dataGridView1.DataSource = dataTable;
+
+
+            //lấy dữ liệu từ NhanVienDao để hiển thị lên datagridview
+
+        }
     }
 }
+
