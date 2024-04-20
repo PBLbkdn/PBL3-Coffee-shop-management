c1620bce6ddc8fbc537c4d7fd0f1437435b9f0f6
Monmoonluna <minhnguyetdn2004@gmail.com>
2024-04-20 18:45:24 +0700

    mon2

--- PBL3/DAO/DALQuanCaPhe.cs
+++ PBL3/DAO/DALQuanCaPhe.cs
@@ -10,7 +10,7 @@ namespace PBL3.DAO
     internal class DALQuanCaPhe : DbContext
     {
         public DALQuanCaPhe() : base("name=QuanCaPhe") {
-            Database.SetInitializer<DALQuanCaPhe>(new CreateDB());
+           // Database.SetInitializer<DALQuanCaPhe>(new CreateDB());
         }
         public virtual DbSet<DTO.NhanVien> NhanViens { get; set; }
         public virtual DbSet<DTO.ChucVu> ChucVus { get; set; }
